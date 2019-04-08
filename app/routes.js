029d27fddee939df16f4c939c4b375f67afd6cf2
mec <mec.rawlings@gmail.com>
2019-04-08 14:30:06 +0100

    Add the first two questions to the journey
    The first question is a simple consent form.
    
    We added a POST route to handle the branching on the second question.
    
    We stubbed the third question to check our branching.
    
    We added a stubbed ineligible page as well.

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,15 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/teacher-when', function (req, res) {
+
+  let eligible = req.session.data['eligible']
+
+  if (eligible === 'false') {
+    res.redirect('/ineligible')
+  } else {
+    res.redirect('/teacher-still-teaching')
+  }
+})
+
 module.exports = router
