5543c1beb335735c5f481cffd0ab9c9114f3ded8
Juan Fernandes <juan@fernandes.fyi>
2019-04-29 12:19:03 +0100

    Removes E prototype folder and merges with main files
    Originally it was thought that the best approach would be to create a
    folder and make changes to the prototype there - but this caused issues
    with the school lookup script. Renaming the page didn't fix the issue.
    
    Speaking with service design, it was decided we don't need to keep the
    prototype as it is and we can make the changes to existing files.

--- app/routes.js
+++ app/routes.js
@@ -9,45 +9,55 @@ router.post('/teacher-when', function (req, res) {
   if (eligible === 'false') {
     res.redirect('/ineligible')
   } else {
-    res.redirect('/teacher-still-teaching')
-  }
-})
-
-router.post('/teacher-still-teaching', function (req, res) {
-  let stillTeaching = req.session.data['still-teaching']
-
-  if (stillTeaching === 'false') {
-    res.redirect('/ineligible')
-  } else {
     res.redirect('/teacher-route')
   }
 })
 
-router.post('/teacher-qualified', function (req, res) {
-  let qualificationRoute = req.session.data['qualification-route']
+router.post('/teacher-route', function (req, res) {
+  let qualificationRoute = req.session.data['qualificationRoute']
 
-  let question = ''
-
-  switch (qualificationRoute) {
-    case 'pgce' :
-      question = 'What subject did you specialise in during your PGCE?'
-      break
-
-    case 'school-direct' :
-      question = 'What subject did you specialise in during your School Direct teacher training?'
-      break
-
-    case 'scitt' :
-      question = 'What subject did you specialise in during your School-centred initial teacher training?'
-      break
-
-    case 'teach-first' :
-      question = 'What subject did you specialise in during your Teach First initial teacher training?'
-      break
+  if (qualificationRoute === 'false') {
+    res.redirect('/ineligible')
+  } else {
+    res.redirect('/teacher-subject')
   }
-
-  res.render('teacher-qualified', { 'questionText': question })
 })
+
+router.post('/teacher-subject', function (req, res) {
+  let teachingSubject = req.session.data['teachingSubject']
+
+  if (teachingSubject === 'false') {
+    res.redirect('/ineligible')
+  } else {
+    res.redirect('/teacher-location')
+  }
+})
+
+// router.post('/teacher-qualified', function (req, res) {
+//   let qualificationRoute = req.session.data['qualification-route']
+
+//   let question = ''
+
+//   switch (qualificationRoute) {
+//     case 'pgce' :
+//       question = 'What subject did you specialise in during your PGCE?'
+//       break
+
+//     case 'school-direct' :
+//       question = 'What subject did you specialise in during your School Direct teacher training?'
+//       break
+
+//     case 'scitt' :
+//       question = 'What subject did you specialise in during your School-centred initial teacher training?'
+//       break
+
+//     case 'teach-first' :
+//       question = 'What subject did you specialise in during your Teach First initial teacher training?'
+//       break
+//   }
+
+//   res.render('teacher-qualified', { 'questionText': question })
+// })
 
 router.post('/teacher-location', function (req, res) {
   let qualifiedSubject = req.session.data['qualified-subject']
@@ -77,46 +87,6 @@ router.post('/teacher-qualified-teach-first-ske', function (req, res) {
   } else {
     res.redirect('/teacher-location')
   }
-})
-
-// Prototype E
-router.post('/e/teacher-when', function (req, res) {
-  let eligible = req.session.data['eligible']
-
-  if (eligible === 'false') {
-    res.redirect('/e/ineligible')
-  } else {
-    res.redirect('/e/teacher-route')
-  }
-})
-
-router.post('/e/teacher-route', function (req, res) {
-  let qualificationRoute = req.session.data['qualificationRoute']
-
-  if (qualificationRoute === 'false') {
-    res.redirect('/e/ineligible')
-  } else {
-    res.redirect('/e/teacher-subject')
-  }
-})
-
-router.post('/e/teacher-subject', function (req, res) {
-  let teachingSubject = req.session.data['teachingSubject']
-
-  if (teachingSubject === 'false') {
-    res.redirect('/e/ineligible')
-  } else {
-    res.redirect('/e/teacher-school')
-  }
-})
-
-router.get('/e/teacher-school', function (req, res) {
-  var schoolData = require('./data/gias_all.min.json')
-  var schoolList = schoolData.map(function (school) {
-    return school.est_name
-  })
-
-  res.render('teacher-school', { 'schoolList': schoolList })
 })
 
 module.exports = router
