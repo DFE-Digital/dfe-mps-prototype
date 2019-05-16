eea035bff7f7674c472457f5aa8acbf8c6a845af
Mark Goddard <mark@paper.studio>
2019-05-16 15:18:04 +0100

    New service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Teachers: claim retention payments',
+  serviceName: 'Claim a payment for teaching mathematics or physics',
 
   // Default port that prototype runs on
   port: '3000',
