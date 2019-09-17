faa49e9ed5c45085dcf352a170ebb42b30538b7b
Rich Cooley <rich@dxw.com>
2019-09-17 13:41:16 +0100

    Amend service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Claim a payment for teaching mathematics or physics",
+  serviceName: "Claim a payment for teaching maths or physics",
 
   // Default port that prototype runs on
   port: "3000",
